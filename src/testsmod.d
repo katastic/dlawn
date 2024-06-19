6a56002deb3f22659b0a0a7805bce820522f4fab
katastic <ckatko@gmail.com>
2024-06-19 15:23:27 -0500

    remote repo

--- src/testsmod.d
+++ src/testsmod.d
@@ -2,8 +2,7 @@ import std.stdio;
 import g;
 import molto;
 
-void runConsoleTests() /// Test D stuff without loading the entire game.
-{
+void runConsoleTests() { /// Test D stuff without loading the entire game.
 	writeln("Running console tests");
 
 	statistics_t s;
@@ -17,13 +16,11 @@ void runConsoleTests() /// Test D stuff without loading the entire game.
 	writeln(s["tacos"]);
 }
 
-void runAllegroTests() /// Test stuff after Allegro sets up display, etc.
-{
+void runAllegroTests(){ /// Test stuff after Allegro sets up display, etc.
 	writeln("Running Allegro tests");
 	//	bitmapHandler bh = new bitmapHandler();
 	//	bh.loadJSON();
 
 	writeln(splitStringAtWidth(
 			"test stuff after allegro sets up display, and does other stuff.", 200));
-
 }
